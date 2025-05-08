5bbb2871f082127a29dd6f3b077a478001ed0d79
shellmonk <root@shellmonk.io>
2025-05-08 23:58:44 +0200

    Finally started doing some work on the regex parser

--- tseger/src/parser.rs
+++ tseger/src/parser.rs
@@ -1,7 +1,22 @@
+// TODO: Remove this when module is ready
+#![allow(dead_code)]
+
+use crate::common::TsegerError;
+use crate::lexer::Position;
 use crate::lexer::RegexAtom;
 
-pub fn parse(tokens: Vec<RegexAtom>) -> AST {
-    AST::new()
+pub struct Parser<'a> {
+    tokens: &'a Vec<(RegexAtom, Position)>,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(tokens: &'a Vec<(RegexAtom, Position)>) -> Self {
+        Self { tokens }
+    }
+
+    pub fn parse(&self) -> Result<AST, TsegerError> {
+        Ok(AST::new())
+    }
 }
 
 pub struct AST {}
